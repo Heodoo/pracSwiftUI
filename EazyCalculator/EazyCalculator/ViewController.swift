2f1500a40620bdf59f45a9d2d42e74717d7de802
heodoo <fgv23@ajou.ac.kr>
2022-10-13 16:30:15 +0900

    add EazyCalculator

--- EazyCalculator/EazyCalculator/ViewController.swift
+++ EazyCalculator/EazyCalculator/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let syntherzier: AVSpeechSynthesizer = AVSpeechSynthesizer()
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func pressCalcButton(_ sender: Any) {
+        let myString: String = myTextField.text ?? "0"
+        let myNumber: Int = Int(myString) ?? 0
+        
+        print("\(myNumber)")
+        let result: Int = myNumber*myNumber
+        let resultString : String = "결과: \(result)"
+        myLabel.text = resultString
+        syntherzier.speak(AVSpeechUtterance(string: resultString + " 입니다."))
+        
+    }
+    
 
 }
 
