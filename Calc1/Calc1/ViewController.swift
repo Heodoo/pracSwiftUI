1a51bcb3aee9cad1f6a0013360cc50cf709c08af
heodoo <fgv23@ajou.ac.kr>
2022-10-14 17:04:24 +0900

    add Calc1

--- Calc1/Calc1/ViewController.swift
+++ Calc1/Calc1/ViewController.swift
@@ -8,7 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number: Int = 0
+    
+    @IBOutlet weak var insertNumberLabel: UILabel!
+    
+    
+    
+    @IBAction func touchButton(_ sender : UIButton){
+        guard let inputText = sender.titleLabel?.text
+        else{ return }
+        print(inputText)
+        var inputNumber = Int(inputText) ?? 0
+        number = 10*number + inputNumber
+        insertNumberLabel.text = "\(number)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
