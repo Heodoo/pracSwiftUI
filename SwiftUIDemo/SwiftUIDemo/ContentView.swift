db2997b0bc0dd1b4d1b86f6e85e4ef67ff9151ac
heodoo <fgv23@ajou.ac.kr>
2022-10-19 19:43:35 +0900

    add SwiftUIDemo (AsyncImage,Picker,Binding)

--- SwiftUIDemo/SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -7,13 +7,57 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    var colors:[Color] = [.black,.red,.green, .blue]
+    var colornames = ["Black","Red","Green","Blue"]
+    
+    @State private var colorIndex = 0
+    @State private var rotation: Double = 0
+    @State private var text: String = "Welcome to SwiftUI"
+
+    let imageURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/%EB%A9%8B%EC%9F%81%EC%9D%B4%EC%82%AC%EC%9E%90%EC%B2%98%EB%9F%BC_%EB%A1%9C%EA%B3%A0.png/692px-%EB%A9%8B%EC%9F%81%EC%9D%B4%EC%82%AC%EC%9E%90%EC%B2%98%EB%9F%BC_%EB%A1%9C%EA%B3%A0.png"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            Spacer()
             Text("Hello, world!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .rotationEffect(.degrees(self.rotation))
+                .animation(.easeInOut(duration: 5))
+                .foregroundColor(colors[colorIndex])
+            Spacer()
+            Divider()
+            Slider(value: $rotation, in: 0...360, step: 0.1)
+                .padding()
+            
+            TextField("Enter text here",text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            HStack{
+                Text("Color")
+                    .foregroundColor(colors[colorIndex])
+                Picker(selection: $colorIndex, label: Text("Color")) {
+                    ForEach (0..<colornames.count){
+                        Text(colornames[$0])
+                            .foregroundColor(colors[$0])
+                            .tag(colors[$0])
+                    }
+                }.pickerStyle(.menu)
+
+                AsyncImage(url: URL(string: imageURL)){
+                    $0.resizable()
+                } placeholder: {
+                    //이미지를 데이터 통신하면서 아직 데이터가 오지 않았을때, 준비중이라고 표시
+                    ProgressView()
+                }
+                .frame(width: 200, height: 200)
+                
+            }
+            .padding()
+            
         }
         .padding()
     }
