c4ac93a33017f86182d938ef3351ef3b42bb41e8
Heodoo <39407635+Heodoo@users.noreply.github.com>
2022-12-04 21:45:53 +0900

    애플티비(tvOS) 앱 맛보기

---
+++ PractvOS/PractvOS/PractvOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PractvOSApp.swift
+//  PractvOS
+//
+//  Created by MacBook on 2022/12/01.
+//
+
+import SwiftUI
+
+@main
+struct PractvOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
